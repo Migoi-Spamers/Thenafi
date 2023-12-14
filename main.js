4baf5d2b992bea5bac62d2ac0b4b28d294bad1fc
Dattt <thanhdat240220@gmail.com>
2023-12-14 23:02:20 +0700

    add buyback the token

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const fs = require('fs');
 const axios = require("axios");
 const {Web3} = require('web3');
 
@@ -16,8 +17,13 @@ const {
 const TheAddress = '0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11';
 const liveTheAddress = '0xCdC3A010A3473c0C4b2cB03D8489D6BA387B83CD';
 
+const theContractABI = require('./theABI.json');
+const liveTheContractABI = require('./liveTheABI.json');
 const contractABI = require('./liveThePoolABI.json');
-const tokenContractABI = require('./theABI.json');
+
+const THE_NAME = 'THE';
+const LIVETHE_NAME = 'LIVETHE';
+
 const contractAddress = '0xd4ae6eCA985340Dd434D38F470aCCce4DC78D109';
 
 const web3 = new Web3(BSC_RPC);
@@ -26,9 +32,12 @@ const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
 web3.eth.accounts.wallet.add(account);
 web3.eth.defaultAccount = account.address;
 
-const tokenContract = new web3.eth.Contract(tokenContractABI, TheAddress);
+const theContract = new web3.eth.Contract(theContractABI, TheAddress);
+const liveTheContract = new web3.eth.Contract(liveTheContractABI, liveTheAddress);
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+let swapsToLiveThe = true;
 
 function getTokenPrices() {
     return new Promise((resolve, reject) => {
@@ -56,7 +65,8 @@ function getTokenPrices() {
         }
         ).then((response) => {
             const [the, liveThe] = response.data.data.getTokenPrices;
-            const diffPercent = 100 - (liveThe.priceUsd / the.priceUsd) * 100;
+            // const diffPercent = 100 - (liveThe.priceUsd / the.priceUsd) * 100;
+            const diffPercent = Math.floor(Math.random() * 8);
 
             console.log(`
                 Date: ${Date()}
@@ -65,10 +75,24 @@ function getTokenPrices() {
                 LiveTHE/THE: ${diffPercent}%
             `);
 
-            if (diffPercent >= Number.parseFloat(SWAP_PERCENT)) {
-                swapToLiveThe(resolve, reject);
+            const logData = {
+                thePrice: the.priceUsd, 
+                livePrice: liveThe.priceUsd, 
+                percent: diffPercent
+            };
+
+            if (swapsToLiveThe) {
+                if (diffPercent >= Number.parseFloat(SWAP_PERCENT)) {
+                    swap(LIVETHE_NAME, resolve, reject, logData);
+                } else {
+                    resolve({bought: false});
+                }
             } else {
-                resolve({bought: false});
+                if (diffPercent < 2) {
+                    swap(THE_NAME, resolve, reject, logData);
+                } else {
+                    resolve({bought: false});
+                }
             }
         }).catch((error) => {
             reject(error);
@@ -76,15 +100,24 @@ function getTokenPrices() {
     });
 }
 
-async function swapToLiveThe(resolve, reject) {
+async function swap(swapTo, resolve, reject, logData) {
     try {
-        console.log('Swapping THE to liveTHE');
-        const amountIn = Number.parseFloat(AMOUNT_IN) * (10 ** 18); // 1 token in wei (adjust based on token decimals)
-        const amountOutMin = Number.parseFloat(AMOUNT_OUT_MIN) * (10 ** 18); // Minimum acceptable amount of output tokens
+        console.log(`Swapping to ${swapTo}`);
+
+        swapsToLiveThe = !swapsToLiveThe;
+
+        const tokenContract = swapTo === LIVETHE_NAME ? theContract : liveTheContract;
+        let amountIn = await tokenContract.methods.balanceOf(YOUR_ADDRESS).call();
+        amountIn = Number.parseFloat(amountIn) / (10 ** 18);
+        amountIn = (Math.floor(amountIn * 100) / 100) * (10 ** 18);
+
+        const amountOutMin = amountIn * 0.98;
+
+        console.log(amountIn, amountOutMin);
 
         const routes = [{
-            from: TheAddress,
-            to: liveTheAddress,
+            from: swapTo === LIVETHE_NAME ? TheAddress : liveTheAddress,
+            to: swapTo === LIVETHE_NAME ? liveTheAddress : TheAddress,
             stable: true
         }];
 
@@ -94,7 +127,7 @@ async function swapToLiveThe(resolve, reject) {
             gasPrice: web3.utils.toWei('3', 'gwei'),
         });
 
-        console.log('Approved THE token');
+        console.log(`Approved token`);
 
         const deadline = Math.floor(Date.now() / 1000) + 60 * 2;
         contract.methods.swapExactTokensForTokens(JSON.stringify(amountIn), JSON.stringify(amountOutMin), routes, YOUR_ADDRESS, deadline)
@@ -105,7 +138,7 @@ async function swapToLiveThe(resolve, reject) {
             })
             .then((result) => {
                 console.log('Swap successful! txs: ', result.transactionHash);
-                resolve({bought: true});
+                resolve({bought: true, logData: {txsHash: result.transactionHash, swapTo: swapTo, ...logData}});
             })
             .catch((error) => {
                 reject(error);
@@ -123,11 +156,22 @@ async function callToGetPrice() {
     while (true) {
         try {
             const data = await getTokenPrices();
+            console.log(data);
             if (data.bought) {
-                console.log('Done');
-                break;
+                fs.appendFile('./logData.txt', `
+                    Date: ${Date()}
+                    THE: $${data.logData.thePrice}
+                    LiveTHE: $${data.logData.livePrice}
+                    LiveTHE/THE: ${data.logData.percent}%
+                    SwapTo: ${data.logData.swapTo}
+                    txs: https://bscscan.com/tx/${data.logData.txsHash}
+                    ---------------------------
+                `, function (err) {
+                    if (err) throw err;
+                    console.log('Updated LogData!');
+                })
             }
-            await sleep(60000 * 5);
+            await sleep(1000);
         } catch (error) {
             console.error(error);
             break;
